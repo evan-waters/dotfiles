4c4c18d9be6f5f4b100f4f08da8ead40013bd890
Ian Crosser <iancrosser@gmail.com>
2015-10-02 16:01:34 -0600

    Attempting to add a new machine start up script to get me up and running quickly on ubuntu.

---
+++ new-machine.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/
+
+sudo apt-get update
+sudo apt-get install -y vim yakuake xclip curl dconf-cli
+
+mkdir ~/.vim
+mkdir ~/.vim/autoload ~/.vim/bundle ~/.vim/backup ~/.vim/swap ~/.vim/undo
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+vim +PluginInstall +qall
+
