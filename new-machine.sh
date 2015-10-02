3e09ee4dfa32dcf28ae142445ceca2a2f99c7ed6
Ian Crosser <iancrosser@gmail.com>
2015-10-02 16:23:27 -0600

    installing the dotfiles with the new machine script.

--- new-machine.sh
+++ new-machine.sh
@@ -7,7 +7,16 @@ sudo apt-get install -y vim yakuake xclip curl dconf-cli
 
 mkdir ~/.vim
 mkdir ~/.vim/autoload ~/.vim/bundle ~/.vim/backup ~/.vim/swap ~/.vim/undo
+
+cd -
+
+cp .bash_aliases ~/.bash_aliases
+cp .vimrc ~/.vimrc
+cp .profile ~/.profile
+cp .Xmodmap ~/.Xmodmap
+
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 vim +PluginInstall +qall
+
 
